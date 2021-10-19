75da0ef6b742e713c53d78eabfbf86e58c94b6e9
ho.doan <doanho.it.dev@gmail.com>
2021-10-19 13:32:08 +0700

    add key ggcaptche

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const http = require('http');
 const fs = require('fs');
 
-fs.readFile('./index.html', function (err, html) {
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
 
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        console.log(request.headers['user-agent'])
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(3000);
-});
+http.createServer(handleRequest).listen(8000);
